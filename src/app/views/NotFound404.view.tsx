1105fb6e7043a8e3097af0762b2e024ca65cdc68
Gabriel Bobrov <bielbobrov@hotmail.com>
2021-10-11 21:03:14 -0300

    NotFound404

--- src/app/views/NotFound404.view.tsx
+++ src/app/views/NotFound404.view.tsx
@@ -1,7 +1,36 @@
+import { useHistory } from "react-router";
+import styled from "styled-components";
+import notfount from "../../Assets/not_found.svg";
+import Button from "../Components/Button/Button";
 export default function NotFound404() {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Não encontrado - 404</h1>
-    </div>
+    <NotFound404Wrapper>
+      <span>Oops!</span>
+      <h1>Não encontramos esta página</h1>
+      <img src={notfount} alt="Não encontrado"></img>
+      <Button
+        variant="primary"
+        label="Ir para a home"
+        onClick={() => history.replace("/")}
+      />
+    </NotFound404Wrapper>
   );
 }
+
+const NotFound404Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 32px;
+
+  span {
+    font-size: 72px;
+  }
+  h1 {
+    font-size: 18px;
+    font-weight: 400;
+  }
+`;
