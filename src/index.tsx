65b8df9ae5e860643636243c74e8792f09d89007
Gabriel Bobrov <bielbobrov@hotmail.com>
2021-10-19 13:06:36 -0300

    Primeira integracao com a API

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,17 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+function getDataFromApi() {
+  fetch("http://localhost:8080/posts")
+    .then((res) => res.json())
+    .then((posts) => {
+      console.log(posts);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+getDataFromApi();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
