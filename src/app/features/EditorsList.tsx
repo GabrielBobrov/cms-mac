d855c1d02c5092c4111fed2350cfc25af83be886
Gabriel Bobrov <bielbobrov@hotmail.com>
2021-10-21 14:32:55 -0300

    Criando @types

--- src/app/features/EditorsList.tsx
+++ src/app/features/EditorsList.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { User } from "../../@types/User";
 import Profile from "../Components/Profile";
 
 export default function EditorsList() {
+  const editors: User.EditorSummary[] = [];
   return (
     <EditorsListWrapper>
       <Profile
