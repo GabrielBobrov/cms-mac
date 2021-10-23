652fcc74bd79da0d3bc9ce2843b579fd33089853
Gabriel Bobrov <bielbobrov@hotmail.com>
2021-10-23 19:43:35 -0300

    FileService

---
+++ src/sdk/services/File.service.ts
@@ -0,0 +1,11 @@
+import { File } from "../@types";
+import Service from "../Service";
+
+class FileService extends Service {
+  static getSignedUrl(fileInfo: File.UploadRequestInput) {
+    return this.Http.post<File.UploadRequest>("/upload-request", fileInfo)
+      .then(this.getData)
+      .then((res) => res.uploadSignedUrl);
+  }
+}
+export default FileService;
