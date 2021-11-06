1b44d853a66b4df316fd7e40c953c1124e51711c
Gabriel Bobrov <bielbobrov@hotmail.com>
2021-11-06 14:57:16 -0300

    Integrando chart

--- src/app/features/UserPerformance.tsx
+++ src/app/features/UserPerformance.tsx
@@ -1,44 +1,23 @@
-import Chart from "../Components/Chart/Chart";
+import { useState, useEffect } from "react";
+import MetricService from "../../sdk/services/Metric.service";
+import transformEditorMonthlyEarningsIntoChartJs from "../../sdk/utils/transformEditorMonthlyEarningsIntoChartJs";
+import Chart, { ChartProps } from "../Components/Chart/Chart";
 
-const FAKE_DATA = {
-  labels: [
-    "Batata",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ],
-  datasets: [
-    {
-      label: "Receitas",
-      data: [500, 400, 600, 100, 800, 20, 123, 320, 120, 500, 434, 322],
-      fill: true,
-      backgroundColor: "#0099FF",
-      borderColor: "#0099FF",
-      borderWidth: 0.5,
-      yAxisID: "cashflow",
-    },
-    {
-      label: "Despesas",
-      data: [100, 200, 250, 500, 1000, 600, 123, 210, 344, 800, 123, 0],
-      fill: true,
-      backgroundColor: "#274060",
-      borderColor: "#274060",
-      borderWidth: 0.5,
-      yAxisID: "cashflow",
-    },
-  ],
-};
+export default function UserPerformance() {
+  const [editorEarnings, setEditorEarings] = useState<ChartProps["data"]>();
 
-export default function UserMetrics() {
+  useEffect(() => {
+    MetricService.getEditorMonthlyEarnings()
+      .then(transformEditorMonthlyEarningsIntoChartJs)
+      .then(setEditorEarings);
+  }, []);
+
+  if (!editorEarnings) return null;
+
   return (
-    <Chart title="Média de performance nos últimos 12 meses" data={FAKE_DATA} />
+    <Chart
+      title="Média de performance nos últimos 12 meses"
+      data={editorEarnings}
+    />
   );
 }
